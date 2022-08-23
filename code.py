de2d3eb4e5f5c62602eb1fe9e1432dbf2235a71c
ajaykr-07 <111410827+ajaykr-07@users.noreply.github.com>
2022-08-23 10:09:23 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,8 @@
+import random
+Head_or_Tail=["Head","Tail"]
+number=radom.randint(0,1)
+your_call=input("Head or Tail :")
+if (your_call==Head_or_Tail[number]):
+  print("you won")
+else:
+  print("Sorry, You lost")
